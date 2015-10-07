cbcde11dd20bef5245d97c08c063c116694fa7c9
Kuei-Hsien Chu <guixien@gmail.com>
2015-10-07 16:06:51 -0500

    AnimalPen updated

---
+++ Inheritance/AnimalPen.cpp
@@ -0,0 +1,25 @@
+#include "AnimalPen.h"
+
+AnimalPen::AnimalPen()
+{
+}
+
+AnimalPen::~AnimalPen()
+{
+}
+
+void AnimalPen::AddAnimal(FarmAnimal* animal)
+{
+}
+
+FarmAnimal* AnimalPen::peekAtNextAnimal()
+{
+}
+
+void AnimalPen::releaseAnimal()
+{
+}
+
+bool isPenEmpty()
+{
+}
