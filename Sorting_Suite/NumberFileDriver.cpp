fa83576201e01d1e1acad2d86f2298fcc4b3640a
Kuei-Hsien Chu <guixien@gmail.com>
2015-10-28 12:47:14 -0500

    argv and argc

--- Sorting_Suite/NumberFileDriver.cpp
+++ Sorting_Suite/NumberFileDriver.cpp
@@ -6,6 +6,22 @@ void NumberFileDriver::run(int argc, char** argv)
 
 void NumberFileDriver::printHelpMenu()
 {
+	std::cout << "\nUse Number File Generator in one of the following ways:\n\n"
+			<< "./prog -create -a filename amount\n"
+			<< "./prog -create -d filename amount\n"
+			<< "./prog -create -s filename amount value\n"
+			<< "./prog -create -r filename amount min max\n"
+			<< "Option explainations:\n"
+			<< "\t-a for ascending\n"
+			<< "\t-d for descending\n"
+			<< "\t-s for a single value\n"
+			<< "\t-r for random values\n"
+			<< "\tfilename is the ouput file name\n"
+			<< "\tamount is the amount of random numbers to place in the file\n"
+			<< "\tvalue is the single number that will be written to file in -s mode\n"
+			<< "\tmin is the low end of the range of random numbers\n"
+			<< "\tmax is the high end (non-inclusive) of the range of random numbers\n";
+
 }
 
 bool NumberFileDriver::isValidOption(std::string option)
